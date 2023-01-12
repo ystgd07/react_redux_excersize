26a4543906f6af328465a32530613cf65103b5f0
yang <ysotgood@gmail.com>
2023-01-13 07:50:55 +0900

    리덕스 툴킷 쿼리 에러 발생점

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,30 @@
+import { useFetchAlbumsQuery,useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./Expandalbepanel";
+import Button from './Button'
 function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const {data,error,isLoading}=useFetchAlbumsQuery(user)
+  const [addAlbum,resultes]=useAddAlbumMutation()
+  const handleAddAlbum=()=>{
+    addAlbum(user)
+  }
+  let content
+  if(isLoading){
+    content=<Skeleton times={3}/>
+
+  }else if(error){
+    content=<div>Error loading albums.</div>
+  }else{
+    content=data.map(album=>{
+      const header=<div>{album.title}</div>
+      return<ExpandablePanel key={album.id} header={header}>
+        List of posts in the album
+      </ExpandablePanel>
+    })
+  }
+  return <div>
+    <div>Albums for {user.name}
+    <Button onClick={handleAddAlbum}>click!</Button></div>Albums for {user.name}</div>;
 }
 
 export default AlbumsList;
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { userReducer } from "./slices/usersSlice";
-
+import { albumsApi } from "./apis/albumsApi";
 export const store = configureStore({
   reducer: {
     users: userReducer,
+    [albumsApi.reducerPath]:albumsApi.reducer
   },
+  middleware:(getDefaultMiddleWare)=>{
+    return getDefaultMiddleWare().concat(albumsApi.middleware)
+  }
 });
+
+setupListeners(store.dispatch)
+
 
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUsers";
 export * from "./thunks/removeUser";
+export {useFetchAlbumsQuery,useAddAlbumMutation} from './apis/albumsApi'
